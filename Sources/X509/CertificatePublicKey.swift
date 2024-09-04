7e85e7d5071f2333e3b36c2f9df8f3150bb8462f
Basile Cornet <basile@cornet.im>
2024-09-05 00:16:12 +0200

    Parameter of EC Key

--- Sources/X509/CertificatePublicKey.swift
+++ Sources/X509/CertificatePublicKey.swift
@@ -253,6 +253,24 @@ extension Certificate.PublicKey {
         SubjectPublicKeyInfo(self).key.bytes
     }
     @inlinable
+    public var parameter: String {
+        let desc = SubjectPublicKeyInfo(self).algorithmIdentifier.parameters
+        var serializer = DER.Serializer()
+        try? desc?.serialize(into: &serializer)
+        let object = try? ASN1ObjectIdentifier(derEncoded: serializer.serializedBytes)
+
+        switch object {
+        case .NamedCurves.secp256r1:
+            return "secp256r1"
+        case .NamedCurves.secp384r1:
+            return "secp384r1"
+        case .NamedCurves.secp521r1:
+            return "secp521r1"
+        default: return desc?.description ?? ""
+        }
+    }
+
+    @inlinable
     public var modulus: ArraySlice<UInt8>? {
         let bytes = SubjectPublicKeyInfo(self).key.bytes
         if let key = try? RSAPKCS1PublicKey(derEncoded: bytes) {
